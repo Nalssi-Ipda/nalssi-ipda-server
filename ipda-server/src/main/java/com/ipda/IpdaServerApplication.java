83a47f38f78a0bfb93535894eb7bb97471e1ebe4
JangBJ <qudwnd1001@naver.com>
2026-09-19 13:12:19 +0900

    🔥hotfix: 서버 언어 변경

---
+++ ipda-server/src/main/java/com/ipda/IpdaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ipda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpdaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpdaServerApplication.class, args);
+	}
+
+}
---
+++ ipda-server/src/test/java/com/ipda/IpdaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpdaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
